ebce646b7e4e18eab850f279ae3ebe80295cfe84
Burgo1c <72175382+Burgo1c@users.noreply.github.com>
2023-08-02 13:36:52 +0900

    Update config.php
    Removed IP and login info.

--- common/config.php
+++ common/config.php
@@ -3,20 +3,20 @@
 /**
  * DATABASE CONNECTION
  */
-define("DB_CON_STR", "oci:dbname=(description=(address=(protocol=tcp)(host=192.168.5.100)(port=1521))(connect_data=(service_name=ORCL)));charset=AL32UTF8");
+define("DB_CON_STR", "oci:dbname=(description=(address=(protocol=tcp)(host=000.000.000.000)(port=0000))(connect_data=(service_name=name)));charset=AL32UTF8");
 //
-define("DB_USER", "eawc");
-define("DB_PASS", "eawc");
+define("DB_USER", "user");
+define("DB_PASS", "user");
 
 /**
  * PRODUCT FILE PATH
  */
 define("PRODUCT_FILE_PATH", "temp/");
 define("PRODUCT_FILE_NAME", "Product.csv");
-define("PRODUCT_IMPORT_FILE_PATH", "\\\\192.168.5.70\\eawc-share");
+define("PRODUCT_IMPORT_FILE_PATH", "\\\\000.000.000.000\\folder");
 define("PRODUCT_IMPORT_FILE_NAME", "S1ITEM*.txt");
-define("PRODUCT_IMPORT_USER", "administrator");
-define("PRODUCT_IMPORT_PASS", "kunimo#10");
+define("PRODUCT_IMPORT_USER", "user");
+define("PRODUCT_IMPORT_PASS", "password");
 
 /**
  * SIGNAGE IP
@@ -31,14 +31,14 @@ define("ST_2311_IP", "192.168.5.90");
 /**
  * ENDJ02 AND wait_msg FILE PATH
  */
-define("FILE_PATH", "\\\\192.168.5.72\\KSD\\EXE");
+define("FILE_PATH", "\\\\000.000.000.000\\folder\\folder");
 define("END_FILE_NAME", "\\ENDJ02");
 define("MSG_FILE_NAME", "\\wait_msg.txt");
-define("FOLDER_USER", "administrator");
-define("FOLDER_PASS", "kunimo#10");
+define("FOLDER_USER", "user");
+define("FOLDER_PASS", "password");
 define("TEMP_PATH", "D:/htdocs/signage/temp/");
 
 /**
  * HANDY用 API ENDPOINT
  */
-define("API_ENDPOINT", "http://164.70.88.229/index.php?d=");
+define("API_ENDPOINT", "http://000.000.000.000/index.php?d=");
